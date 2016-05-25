d15e7c9cd0e5abdaa978c3da52e7d49797476193
Anjani Phuyal <anjani.phuyal@rackspace.co.uk>
2016-05-25 15:48:04 +0100

    Adding myql user creation

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -17,3 +17,36 @@ end
 mysql_client 'default' do
   action :create
 end
+
+mysql2_chef_gem 'default' do
+  action :install
+end
+
+# Create a mysql database
+mysql_database 'lms_tna' do
+  connection(
+    :host     => '127.0.0.1',
+    :username => 'root',
+    :password => 'a'
+  )
+  action :create
+end
+
+
+# create connection info as an external ruby hash
+mysql_connection_info = {
+  :host     => '127.0.0.1',
+  :username => 'root',
+  :password => 'a'
+}
+
+# Create a mysql user
+# Grant SELECT, UPDATE, and INSERT privileges to all tables in foo db from all hosts
+mysql_database_user 'lms_tna' do
+  connection    mysql_connection_info
+  password      'super_secret'
+  database_name 'lms_tna'
+  host          '%'
+  privileges    [:select,:update,:insert]
+  action        :grant
+end
